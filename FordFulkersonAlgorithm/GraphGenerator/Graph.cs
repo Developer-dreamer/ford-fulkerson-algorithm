164712426af55b2e7be6e4e3aab6a11f24da8dd0
Serafym Podolianchuk <70523937+Developer-dreamer@users.noreply.github.com>
2024-04-18 00:03:08 +0300

    added List type graph generation

--- FordFulkersonAlgorithm/GraphGenerator/Graph.cs
+++ FordFulkersonAlgorithm/GraphGenerator/Graph.cs
@@ -49,6 +49,28 @@ public class Graph
 
     private List<List<int>> GenerateList(int vertices, double density)
     {
+        _graphList = new List<List<int>>();
+        var random = new Random();
+
+        for (int i = 0; i < vertices; i++)
+        {
+            _graphList.Add(new List<int>());
+        }
+
+        for (int i = 0; i < vertices; i++)
+        {
+            for (int j = 0; j < vertices; j++)
+            {
+                if (i != j && random.NextDouble() < density)
+                {
+                   _graphList[i].Add(j);
+                }
+                else
+                {
+                    _graphList[i].Add(0);
+                }
+            }
+        }
         return new List<List<int>>();
     }
 }
--- FordFulkersonAlgorithm/Program.cs
+++ FordFulkersonAlgorithm/Program.cs
@@ -1,21 +1,15 @@
 ﻿using FordFulkersonAlgorithm;
+using FordFulkersonAlgorithm.GraphGenerator;
 public class Program
 {
     public static void Main()
     {
         Console.WriteLine("Hello World");
-        int[,] graph = 
-        {
-            { 0, 16, 13, 0, 0, 0 },
-            { 0, 0, 10, 12, 0, 0 },
-            { 0, 4, 0, 0, 14, 0 },
-            { 0, 0, 9, 0, 0, 20 },
-            { 0, 0, 0, 7, 0, 4 },
-            { 0, 0, 0, 0, 0, 0 }
-        };
+        var graph = new Graph(200, 0.02, 'm');
+        var graphMatrix = graph._graphMatrix;
 
         
-        FordFulkerson m = new FordFulkerson(graph);
+        FordFulkerson m = new FordFulkerson(graphMatrix);
         Console.WriteLine("The maximum possible flow is " + FordFulkerson.FordFulkersonAlgorithm(0, 5));
     }
 }
