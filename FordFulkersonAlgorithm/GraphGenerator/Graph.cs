45fb7ed523f7a3ec4798905b447be228fb30b3a2
Serafym Podolianchuk <70523937+Developer-dreamer@users.noreply.github.com>
2024-04-17 23:47:46 +0300

    added graph matrix generation

--- FordFulkersonAlgorithm/GraphGenerator/Graph.cs
+++ FordFulkersonAlgorithm/GraphGenerator/Graph.cs
@@ -2,18 +2,53 @@ namespace FordFulkersonAlgorithm.GraphGenerator;
 
 public class Graph
 {
-    private int[,] _graphMatrix;
+    public int[,] _graphMatrix;
+    private List<List<int>> _graphList;
     private int _verticesAmount;
-    private int _density;
-    public Graph(int n, int p)
+    private double _density;
+    public Graph(int n, double p, char generationType)
     {
         _verticesAmount = n;
         _density = p;
-        _graphMatrix = Generate(_verticesAmount, _density);
+        switch (generationType)
+        {
+            case 'm':
+                _graphMatrix = GenerateMatrix(_verticesAmount, _density);
+                break;
+            case 'l':
+                _graphList = GenerateList(_verticesAmount, _density);
+                break;
+            default:
+                _graphMatrix = GenerateMatrix(_verticesAmount, _density);
+                break;
+        }
     }
 
-    private int[,] Generate(int vertices, int density)
+    private int[,] GenerateMatrix(int vertices, double density)
     {
+       _graphMatrix = new int[vertices, vertices];
+
+        var random = new Random();
+
+        for (int i = 0; i < vertices; i++ )
+        {
+            for (int j = 0; j < vertices; j++)
+            {
+                if ( random.NextDouble() < density)
+                {
+                    _graphMatrix[i, j] = random.Next(1, 51);
+                }
+                else
+                {
+                    _graphMatrix[i, j] = 0;
+                }
+            }
+        }
         return _graphMatrix;
+    }
+
+    private List<List<int>> GenerateList(int vertices, double density)
+    {
+        return new List<List<int>>();
     }
 }
